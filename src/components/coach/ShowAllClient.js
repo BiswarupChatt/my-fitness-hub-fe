9417787efb899d1da65e205c1c905e28a05452aa
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-07-30 20:59:13 +0530

    show all client update

--- src/components/coach/ShowAllClient.js
+++ src/components/coach/ShowAllClient.js
@@ -1,24 +1,53 @@
 import { useState, useEffect } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, TablePagination, Paper, TextField, Container, Grid, Button, Typography, Divider, Modal, Box, CircularProgress, FormControl, MenuItem, Select } from '@mui/material'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination, Paper, TextField, Container, Grid, Button, Typography, Divider, Modal, Box, CircularProgress, FormControl, MenuItem, Select, Avatar, Tooltip, InputLabel } from '@mui/material'
 import { useFormik } from 'formik';
 import { inviteClientValidation } from '../../validations/inviteClientValidations';
-import { loadingToast, updateToast } from '../../utils/toastify';
+import { loadingToast, updateToast, errorToast } from '../../utils/toastify';
 import axios from '../../services/api/axios';
 import moment from 'moment';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
+import HelpIcon from '@mui/icons-material/Help';
 
 const modalStyle = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: {
+        xs: '90%', sm: '80%', md: '60%', lg: '50%', xl: '30%',
+    },
     bgcolor: 'background.paper',
     boxShadow: 24,
     borderRadius: 2,
     p: 4,
 };
 
+const ProgramStatus = ({ program }) => {
+    if (program?.isActive !== undefined) {
+        return program.isActive ? (
+            <Tooltip title="Program is Active">
+                <CheckCircleIcon sx={{ color: 'green', cursor: 'pointer' }} />
+            </Tooltip>
+        ) : (
+            <Tooltip title="Program is not Active">
+                <CancelIcon sx={{ color: 'red', cursor: 'pointer' }} />
+            </Tooltip>
+        )
+    }
+    return (
+        <Tooltip title="Program Not Assigned Yet">
+            <HelpIcon sx={{ color: 'grey', cursor: 'pointer' }} />
+        </Tooltip>
+    )
+}
 
+const AvatarDisplay = ({ user }) => {
+    if (user.profilePicture) {
+        return <Avatar src={user.profilePicture} alt={user.firstName} />;
+    }
+    return <Avatar alt={user.firstName}>{user.firstName.charAt(0).toUpperCase()}</Avatar>;
+}
 
 
 export default function ShowAllClients({ user }) {
@@ -32,6 +61,8 @@ export default function ShowAllClients({ user }) {
     const [totalClients, setTotalClients] = useState(0)
     const [sortBy, setSortBy] = useState('createdAt')
     const [sortOrder, setSortOrder] = useState('asc')
+    const [totalPages, setTotalPages] = useState(null)
+    const [currentPages, setCurrentPages] = useState(null)
     const [search, setSearch] = useState('')
     const [open, setOpen] = useState(false)
     const [isSubmitting, setIsSubmitting] = useState(false)
@@ -52,10 +83,6 @@ export default function ShowAllClients({ user }) {
         });
     };
 
-    const initialValues = {
-        email: ''
-    }
-
     const fetchClients = async () => {
         setLoading(true)
         try {
@@ -71,10 +98,21 @@ export default function ShowAllClients({ user }) {
                     search: search
                 }
             });
+            console.log('response', response)
             setClients(response.data.client)
             setTotalClients(response.data.totalClients)
+            setTotalPages(response.data.totalPages)
+            setCurrentPages(response.data.currentPages)
         } catch (err) {
-            console.error('Error fetching data:', err);
+            console.error('Error fetching data:', err)
+            if (err.response) {
+                const errorMessage = err.response.data.errors?.[0]?.msg || err.response.data.errors || 'An error occurred'
+                errorToast(errorMessage)
+            } else if (err.request) {
+                errorToast('No response from server')
+            } else {
+                errorToast('An unknown error occurred')
+            }
         } finally {
             setLoading(false);
         }
@@ -82,6 +120,7 @@ export default function ShowAllClients({ user }) {
 
     useEffect(() => {
         fetchClients()
+
     }, [page, rowsPerPage, sortBy, sortOrder, token])
 
     const handleSearchSubmit = (e) => {
@@ -91,7 +130,7 @@ export default function ShowAllClients({ user }) {
     }
 
     const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
-        initialValues: initialValues,
+        initialValues: { email: '' },
         validationSchema: inviteClientValidation,
         onSubmit: async (value) => {
             try {
@@ -111,13 +150,10 @@ export default function ShowAllClients({ user }) {
 
                 if (err.response) {
                     const errorMessage = err.response.data.errors?.[0]?.msg || err.response.data.errors || 'An error occurred'
-                    console.log('2')
                     updateToast(errorMessage, 'client-invite-toast', 'error')
                 } else if (err.request) {
-                    console.log('3')
                     updateToast('No response from server', 'client-invite-toast', 'error')
                 } else {
-                    console.log('4')
                     updateToast('An unknown error occurred', 'client-invite-toast', 'error')
                 }
             } finally {
@@ -127,7 +163,6 @@ export default function ShowAllClients({ user }) {
             }
         }
     });
-
 
     return (
         <Container id="clients" sx={{ py: { xs: 8, sm: 4 } }}>
@@ -145,8 +180,9 @@ export default function ShowAllClients({ user }) {
             </Grid>
 
             <Paper>
-                <Grid sx={{ margin: '20px', display: 'flex', alignItems: 'center', justifyContent: 'space-between', }} >
-                    <FormControl variant="outlined" component={'form'} onSubmit={handleSearchSubmit} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', ml: 2, marginTop: 3 }}>
+
+                <Grid sx={{ margin: '20px', display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: { xs: 'column', md: 'row' } }} >
+                    <FormControl variant="outlined" component={'form'} onSubmit={handleSearchSubmit} sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, alignItems: 'center', m: 2 }}>
                         <TextField
                             label="Search"
                             variant="outlined"
@@ -154,14 +190,19 @@ export default function ShowAllClients({ user }) {
                             onChange={(e) => {
                                 setSearch(e.target.value)
                             }}
+                            fullWidth
                         />
-                        <Button variant="contained" color="primary" type='submit' sx={{ ml: 2 }}>
+                        <Button variant="contained" color="primary" type='submit' sx={{ m: 2 }} fullWidth>
                             Search
                         </Button>
                     </FormControl>
                     <Grid >
-                        <FormControl variant="outlined" sx={{ mr: 2, marginTop: 3 }}>
+                        <FormControl variant="outlined" sx={{ m: 2 }}>
+                            <InputLabel id="sort-by">Sort by</InputLabel>
                             <Select
+                                labelId="sort-by"
+                                id="sort-by"
+                                label="Sort By"
                                 value={sortBy}
                                 onChange={(e) => {
                                     setSortBy(e.target.value)
@@ -173,8 +214,12 @@ export default function ShowAllClients({ user }) {
                                 <MenuItem value="email">Email</MenuItem>
                             </Select>
                         </FormControl>
-                        <FormControl variant="outlined" sx={{ mr: 2, marginTop: 3 }}>
+                        <FormControl variant="outlined" sx={{ m: 2 }}>
+                            <InputLabel id="sort-order">Sort Order</InputLabel>
                             <Select
+                                labelId="sort-order"
+                                id="sort-order"
+                                label="Sort By"
                                 value={sortOrder}
                                 onChange={(e) => {
                                     setSortOrder(e.target.value)
@@ -186,30 +231,21 @@ export default function ShowAllClients({ user }) {
                         </FormControl>
                     </Grid>
                 </Grid>
+
                 <TableContainer>
                     <Table>
                         <TableHead>
                             <TableRow>
+                                <TableCell></TableCell>
                                 <TableCell>
-                                    {/* <TableSortLabel
-                                        active={sortBy === 'firstName'}
-                                        direction={sortBy === 'firstName' ? sortOrder : 'asc'}
-                                        onClick={() => setSortBy('firstName')}
-                                    > */}
                                     First Name
-                                    {/* </TableSortLabel> */}
                                 </TableCell>
                                 <TableCell>
-                                    {/* <TableSortLabel
-                                        active={sortBy === 'lastName'}
-                                        direction={sortBy === 'lastName' ? sortOrder : 'asc'}
-                                        onClick={() => setSortBy('lastName')}
-                                    > */}
                                     Last Name
-                                    {/* </TableSortLabel> */}
                                 </TableCell>
                                 <TableCell>Email</TableCell>
                                 <TableCell>Created At</TableCell>
+                                <TableCell>Status</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -222,25 +258,36 @@ export default function ShowAllClients({ user }) {
                             ) : (
                                 clients.map((ele, index) => (
                                     <TableRow key={ele._id} sx={{ backgroundColor: index % 2 === 0 ? "#f7f7f7" : "#ffffff" }}>
+                                        <TableCell>
+                                            <AvatarDisplay user={ele.user} />
+                                        </TableCell>
                                         <TableCell>{ele.firstName}</TableCell>
                                         <TableCell>{ele.lastName}</TableCell>
                                         <TableCell>{ele.email}</TableCell>
                                         <TableCell>{moment(ele.createdAt).format("Do MMM YYYY")}</TableCell>
+                                        <TableCell>
+                                            <ProgramStatus program={ele.program} />
+                                        </TableCell>
                                     </TableRow>
                                 ))
                             )}
                         </TableBody>
                     </Table>
                 </TableContainer>
+
                 <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]}
+                    rowsPerPageOptions={[2,5, 10, 25]}
                     component="div"
                     count={totalClients}
                     rowsPerPage={rowsPerPage}
                     page={page}
                     onPageChange={handleChangePage}
                     onRowsPerPageChange={handleChangeRowsPerPage}
+                    labelDisplayedRows={({ from, to, count }) =>
+                        `${from}–${to} of ${count} | Page ${currentPages} of ${totalPages}`
+                    }
                 />
+
             </Paper>
 
             <Modal open={open} onClose={handleToggle}>
@@ -272,7 +319,9 @@ export default function ShowAllClients({ user }) {
                     </Box>
                 </Box>
             </Modal>
+
             <Divider variant="middle" />
+
         </Container>
     );
 }
