add52998c03e163c9a34d217216b90e8e487fd65
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-08-22 11:26:04 +0530

    client side nutrition plan final

--- src/pages/client/Nutrition.js
+++ src/pages/client/Nutrition.js
@@ -1,15 +1,22 @@
 import { Helmet } from "react-helmet"
 import ClientNutritionPlan from "../../components/client/ClientNutritionPlan"
+import { Container, Paper } from "@mui/material"
+
 export default function Nutrition() {
     return (
         <>
             <Helmet>
                 <title>Nutrition</title>
             </Helmet>
-            <h2>Nutrition Screen</h2>
-            <ClientNutritionPlan/>
+            <Container sx={{ py: { xs: 8, sm: 4 } }}>
+                {/* <Paper elevation={3} sx={{ padding: 1 }}> */}
+
+                    <h2>Nutrition Screen</h2>
+                    <ClientNutritionPlan />
+                {/* </Paper> */}
+            </Container>
         </>
 
     )
-    
+
 }
