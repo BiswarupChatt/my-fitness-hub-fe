b46b27fa0612252418b0ee7a70933ab9d0e97845
Biswarup Chatterjee <125545198+BiswarupChatt@users.noreply.github.com>
2025-01-29 20:43:12 +0530

    Update Login.js

--- src/pages/common/Login.js
+++ src/pages/common/Login.js
@@ -36,8 +36,8 @@ export default function Login() {
     const [isSubmitting, setIsSubmitting] = useState(false)
 
     const initialValues = {
-        email: '',
-        password: ''
+        email: 'chatterjeebiswarup61@gmail.com',
+        password: 'secret123'
     }
 
     const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
